a36ef1537cb47f257115a29565d98fb46043eb6d
Farouq Umar <ufakz99@gmail.com>
2020-02-25 15:03:26 +0100

    Vue Calendar App

---
+++ src/app/components/CalendarWeek.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="calendar-week" class="container">
+    <div class="columns is-mobile">
+      <CalendarDay v-for="day in sharedData.seedData" :key="day.id" :day="day" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { store } from "../store";
+import CalendarDay from "./CalendarDay.vue";
+
+export default {
+  name: "CalendarWeek",
+  data() {
+    return {
+      sharedData: store.state
+    };
+  },
+  components: {
+    CalendarDay
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#calendar-week {
+  margin-bottom: 50px;
+  .column {
+    padding: 0 0 0 0;
+  }
+
+  
+}
+</style>
